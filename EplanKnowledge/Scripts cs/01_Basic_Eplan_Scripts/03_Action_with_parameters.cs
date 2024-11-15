c79b24e97c91e7b3427bbea8d2af021d2d9a692d
Christian <covaga94@gmail.com>
2024-11-15 13:06:51 +0100

    renamed:    EplanKnowledge/Scripts cs/01_Basic_Eplan_Scripts/01_Start.cs -> EplanKnowledge/Scripts cs/01_Basic_Eplan_Scripts/00_Start.cs

---
+++ EplanKnowledge/Scripts cs/01_Basic_Eplan_Scripts/03_Action_with_parameters.cs
@@ -0,0 +1,24 @@
+﻿using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.Scripting;
+
+public class ProjectDetailUpdater
+{
+    [Start]
+    public void Function()
+    {
+        CommandLineInterpreter oCLI = new CommandLineInterpreter();
+        ActionCallingContext acc = new ActionCallingContext();
+
+        // Project path with proper string literal formatting
+        acc.AddParameter("PROJECTNAME", @"C:\Projects\EPLAN\EPLAN_Sample_Project.elk");
+        
+        // Update settings
+        acc.AddParameter("UpdateMacros", "1");
+        acc.AddParameter("UpdateIdentifier", "1");
+
+        // Execute detail update action
+        oCLI.Execute("XPlaUpdateDetailAction", acc);
+
+        return;
+    }
+}
---
+++ EplanKnowledge/Scripts cs/01_Basic_Eplan_Scripts/04_Multiple_Actions.cs
@@ -0,0 +1,21 @@
+﻿using System.Windows.Forms;
+using Eplan.EplApi.ApplicationFramework;
+using Eplan.EplApi.Scripting;
+
+public class Class
+{
+    [Start]
+    public void Function()
+    {
+        CommandLineInterpreter oCLI = new CommandLineInterpreter();
+
+        oCLI.Execute("gedRedraw");
+        oCLI.Execute("UpdateSegmentsFilling");
+        oCLI.Execute("XGedClosePage");
+
+        return;
+    }
+}
+
+
+
