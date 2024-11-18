bbb7505f52c47fbf439c88a7d70aa8cfb8147c04
Christian <covaga94@gmail.com>
2024-11-18 13:26:22 +0100

    new file:   EplanKnowledge/Scripts cs/01_Basic_Eplan_Scripts/08_SimpleProgress.cs 	new file:   EplanKnowledge/Scripts cs/02_Advance_Eplan_Scripts/01_Template_formulare.cs 	new file:   EplanKnowledge/Scripts cs/02_Advance_Eplan_Scripts/01_Template_formulare.json

---
+++ EplanKnowledge/Scripts cs/01_Basic_Eplan_Scripts/08_SimpleProgress.cs
@@ -0,0 +1,51 @@
+﻿using Eplan.EplApi.Base;
+using Eplan.EplApi.Scripting;
+using System.Threading;
+
+public class Class
+{
+    [Start]
+    public void Function()
+    {
+        Progress oProgress = new Progress("SimpleProgress");
+        oProgress.SetAllowCancel(true);
+        oProgress.SetAskOnCancel(true);
+        oProgress.SetNeededSteps(3);
+        oProgress.SetTitle("My Progress Bar");
+        oProgress.ShowImmediately();
+
+        if (!oProgress.Canceled())
+        {
+            oProgress.SetActionText("Step 1");
+            oProgress.SetTitle("Title line 1");
+            oProgress.Step(1);
+
+            Thread.Sleep(1000);
+        }
+
+        if (!oProgress.Canceled())
+        {
+            oProgress.SetActionText("Step 2");
+            oProgress.SetTitle("Title line 2");
+            oProgress.Step(1);
+
+            Thread.Sleep(1000);
+        }
+
+        if (!oProgress.Canceled())
+        {
+            oProgress.SetActionText("Step 3");
+            oProgress.SetTitle("Title line 3");
+            oProgress.Step(1);
+
+            Thread.Sleep(1000);
+        }
+
+        oProgress.EndPart(true);
+
+        return;
+    }
+}
+
+
+
